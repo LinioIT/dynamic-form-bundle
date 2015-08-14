29c0d035f2503b5d2396d618594b7df9b41be76f
Israel Sanabria <israel.sanabria@linio.com>
2015-08-13 19:02:04 -0500

    Redeclared Class Constant to String

--- Tests/Form/FormFactoryTest.php
+++ Tests/Form/FormFactoryTest.php
@@ -118,7 +118,7 @@ class FormFactoryTest extends \PHPUnit_Framework_TestCase
 
     public function testIsCreatingFormWithTransformers()
     {
-        $fieldOneMock = $this->prophesize(FormBuilder::class);
+        $fieldOneMock = $this->prophesize('Symfony\Component\Form\FormBuilder');
 
         $formConfiguration = [
             'foo' => [
