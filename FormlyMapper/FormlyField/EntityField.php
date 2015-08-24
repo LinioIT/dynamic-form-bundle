189ad6aecdc9d73dfbb55a4305bfab2c275b4771
Israel Sanabria <israel.sanabria@linio.com>
2015-08-24 14:09:17 -0500

    Refactored formly response into `EntityField` to hidden field

--- FormlyMapper/FormlyField/EntityField.php
+++ FormlyMapper/FormlyField/EntityField.php
@@ -11,45 +11,7 @@ class EntityField extends FormlyField
      */
     public function getFieldConfiguration()
     {
-        $type = 'select';
-        $typeOptions = $type;
-        $expanded = false;
-        $multiple = false;
-
-        if (isset($this->fieldConfiguration['options']['input']) && $this->fieldConfiguration['options']['input'] === true){
-            $type = 'input';
-            $typeOptions = 'number';
-        } else {
-            if (isset($this->fieldConfiguration['options']['expanded'])) {
-                $expanded = $this->fieldConfiguration['options']['expanded'];
-            }
-
-            if (isset($this->fieldConfiguration['options']['multiple'])) {
-                $multiple = $this->fieldConfiguration['options']['multiple'];
-            }
-
-            if ($expanded === true) {
-                $type = 'radio';
-                if ($multiple === true) {
-                    $type = 'checkbox';
-                }
-                $typeOptions = $type;
-            } else {
-                if ($multiple === true) {
-                    $typeOptions = 'multiple';
-                }
-            }
-        }
-
-        $this->formlyFieldConfiguration['type'] = $type;
-        $this->formlyFieldConfiguration['templateOptions']['type'] = $typeOptions;
-
-        if (isset($this->fieldConfiguration['options']['choices'])) {
-            $choices = $this->fieldConfiguration['options']['choices'];
-
-            $this->formlyFieldConfiguration['templateOptions']['options'] = $choices;
-            unset($this->formlyFieldConfiguration['templateOptions']['choices']);
-        }
+        $this->formlyFieldConfiguration['templateOptions']['type'] = 'hidden';
 
         return $this->formlyFieldConfiguration;
     }
--- Tests/FormlyMapper/FormlyField/EntityFieldTest.php
+++ Tests/FormlyMapper/FormlyField/EntityFieldTest.php
@@ -11,7 +11,7 @@ class EntityFieldTest extends \PHPUnit_Framework_TestCase
      */
     protected $formlyField;
 
-    public function testIsAddingEntityInput()
+    public function testIsAddingEntity()
     {
         $fieldConfiguration = [
             'name' => 'entity',
@@ -22,7 +22,6 @@ class EntityFieldTest extends \PHPUnit_Framework_TestCase
                 'label' => 'Entity',
                 'expanded' => false,
                 'multiple' => false,
-                'input' => true,
             ],
             'validation' => [
                 'Symfony\Component\Validator\Constraints\Regex' => [
@@ -36,260 +35,15 @@ class EntityFieldTest extends \PHPUnit_Framework_TestCase
             'key' => 'entity',
             'type' => 'input',
             'templateOptions' => [
-                'type' => 'number',
+                'type' => 'hidden',
                 'label' => 'Entity',
                 'required' => true,
                 'pattern' => '^[0-9]{3}$',
                 'expanded' => false,
                 'multiple' => false,
-                'input' => true,
             ],
             'validation' => [
                 'messages' => 'Invalid entity Id',
-            ],
-        ];
-
-        $this->formlyField->setFieldConfiguration($fieldConfiguration);
-        $this->formlyField->generateCommonConfiguration();
-        $actual = $this->formlyField->getFieldConfiguration();
-
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function testIsAddingEntitySelectChoice()
-    {
-        $fieldConfiguration = [
-            'name' => 'entity',
-            'type' => 'entity',
-            'class' => '',
-            'options' => [
-                'required' => true,
-                'label' => 'Entity',
-                'expanded' => false,
-                'multiple' => false,
-                'choices' => [
-                    [
-                        'value' => '1',
-                        'text' => 'Entity 1',
-                    ],
-                    [
-                        'value' => '2',
-                        'text' => 'Entity 2',
-                    ],
-                    [
-                        'value' => '3',
-                        'text' => 'Entity 3',
-                    ],
-                ],
-            ],
-        ];
-
-        $expected = [
-            'key' => 'entity',
-            'type' => 'select',
-            'templateOptions' => [
-                'type' => 'select',
-                'label' => 'Entity',
-                'required' => true,
-                'expanded' => false,
-                'multiple' => false,
-                'options' => [
-                    [
-                        'value' => '1',
-                        'text' => 'Entity 1',
-                    ],
-                    [
-                        'value' => '2',
-                        'text' => 'Entity 2',
-                    ],
-                    [
-                        'value' => '3',
-                        'text' => 'Entity 3',
-                    ],
-                ],
-            ],
-        ];
-
-        $this->formlyField->setFieldConfiguration($fieldConfiguration);
-        $this->formlyField->generateCommonConfiguration();
-        $actual = $this->formlyField->getFieldConfiguration();
-
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function testIsAddingEntitySelectMultipleChoice()
-    {
-        $fieldConfiguration = [
-            'name' => 'entity',
-            'type' => 'entity',
-            'class' => '',
-            'options' => [
-                'required' => true,
-                'label' => 'Entity',
-                'expanded' => false,
-                'multiple' => true,
-                'choices' => [
-                    [
-                        'value' => '1',
-                        'text' => 'Entity 1',
-                    ],
-                    [
-                        'value' => '2',
-                        'text' => 'Entity 2',
-                    ],
-                    [
-                        'value' => '3',
-                        'text' => 'Entity 3',
-                    ],
-                ],
-            ],
-        ];
-
-        $expected = [
-            'key' => 'entity',
-            'type' => 'select',
-            'templateOptions' => [
-                'type' => 'multiple',
-                'label' => 'Entity',
-                'required' => true,
-                'expanded' => false,
-                'multiple' => true,
-                'options' => [
-                    [
-                        'value' => '1',
-                        'text' => 'Entity 1',
-                    ],
-                    [
-                        'value' => '2',
-                        'text' => 'Entity 2',
-                    ],
-                    [
-                        'value' => '3',
-                        'text' => 'Entity 3',
-                    ],
-                ],
-            ],
-        ];
-
-        $this->formlyField->setFieldConfiguration($fieldConfiguration);
-        $this->formlyField->generateCommonConfiguration();
-        $actual = $this->formlyField->getFieldConfiguration();
-
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function testIsAddingEntityRadioChoice()
-    {
-        $fieldConfiguration = [
-            'name' => 'entity',
-            'type' => 'entity',
-            'class' => '',
-            'options' => [
-                'required' => true,
-                'label' => 'Entity',
-                'expanded' => true,
-                'multiple' => false,
-                'choices' => [
-                    [
-                        'value' => '1',
-                        'text' => 'Entity 1',
-                    ],
-                    [
-                        'value' => '2',
-                        'text' => 'Entity 2',
-                    ],
-                    [
-                        'value' => '3',
-                        'text' => 'Entity 3',
-                    ],
-                ],
-            ],
-        ];
-
-        $expected = [
-            'key' => 'entity',
-            'type' => 'radio',
-            'templateOptions' => [
-                'type' => 'radio',
-                'label' => 'Entity',
-                'required' => true,
-                'expanded' => true,
-                'multiple' => false,
-                'options' => [
-                    [
-                        'value' => '1',
-                        'text' => 'Entity 1',
-                    ],
-                    [
-                        'value' => '2',
-                        'text' => 'Entity 2',
-                    ],
-                    [
-                        'value' => '3',
-                        'text' => 'Entity 3',
-                    ],
-                ],
-            ],
-        ];
-
-        $this->formlyField->setFieldConfiguration($fieldConfiguration);
-        $this->formlyField->generateCommonConfiguration();
-        $actual = $this->formlyField->getFieldConfiguration();
-
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function testIsAddingEntityCheckboxChoice()
-    {
-        $fieldConfiguration = [
-            'name' => 'entity',
-            'type' => 'entity',
-            'class' => '',
-            'options' => [
-                'required' => true,
-                'label' => 'Entity',
-                'expanded' => true,
-                'multiple' => true,
-                'choices' => [
-                    [
-                        'value' => '1',
-                        'text' => 'Entity 1',
-                    ],
-                    [
-                        'value' => '2',
-                        'text' => 'Entity 2',
-                    ],
-                    [
-                        'value' => '3',
-                        'text' => 'Entity 3',
-                    ],
-                ],
-            ],
-        ];
-
-        $expected = [
-            'key' => 'entity',
-            'type' => 'checkbox',
-            'templateOptions' => [
-                'type' => 'checkbox',
-                'label' => 'Entity',
-                'required' => true,
-                'expanded' => true,
-                'multiple' => true,
-                'options' => [
-                    [
-                        'value' => '1',
-                        'text' => 'Entity 1',
-                    ],
-                    [
-                        'value' => '2',
-                        'text' => 'Entity 2',
-                    ],
-                    [
-                        'value' => '3',
-                        'text' => 'Entity 3',
-                    ],
-                ],
             ],
         ];
 
