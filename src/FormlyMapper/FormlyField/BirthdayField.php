39871e136cfede18ecb71c62c4d3df838ece2f8e
Carlos San Juan <carlossanjuanc@gmail.com>
2021-09-15 17:17:10 -0300

    refactor: improve code readability
    [DIS-735]

--- src/FormlyMapper/FormlyField/BirthdayField.php
+++ src/FormlyMapper/FormlyField/BirthdayField.php
@@ -8,6 +8,9 @@ use Linio\DynamicFormBundle\FormlyMapper\FormlyField;
 
 class BirthdayField extends FormlyField
 {
+    const ORDER_ASC = 'asc';
+    const ORDER_DESC = 'desc';
+
     /**
      * {@inheritdoc}
      */
@@ -22,33 +25,36 @@ class BirthdayField extends FormlyField
     protected function buildFieldTypeConfiguration(): void
     {
         $this->formlyFieldConfiguration['templateOptions']['type'] = $this->getFieldType();
+        $options = $this->fieldConfiguration['options'];
+        
+        $order = isset($options['order']) && (strtolower($options['order']) == (self::ORDER_ASC || self::ORDER_DESC))
+        ? $options['order']
+        : self::ORDER_DESC;
 
-        $order = 'desc';
-        if(isset($this->fieldConfiguration['options']['order'])) {
-            $tmpOrder = strtolower($this->fieldConfiguration['options']['order']);
-            if($tmpOrder == 'asc' || $tmpOrder == 'desc') {
-                $order = $tmpOrder;
-            }
-            unset($this->formlyFieldConfiguration['templateOptions']['order']);
+        unset($this->formlyFieldConfiguration['templateOptions']['order']);
+
+        if(!isset($options['minAgeAllowed']) || !isset($options['maxAgeAllowed'])
+            || !is_numeric($options['minAgeAllowed']) || !is_numeric($options['maxAgeAllowed'])
+            || $options['minAgeAllowed'] < 0  ||  $options['maxAgeAllowed'] < 0) {
+            $this->cleanAgeAllowedInTemplateOptionsArray();
+            return;
         }
 
-        if(isset($this->fieldConfiguration['options']['minAgeAllowed']) && isset($this->fieldConfiguration['options']['maxAgeAllowed'])) {
-            $minAgeAllowed = $this->fieldConfiguration['options']['minAgeAllowed'];
-            $maxAgeAllowed = $this->fieldConfiguration['options']['maxAgeAllowed'];
-            if(is_numeric($minAgeAllowed) && ($minAgeAllowed >= 0) && is_numeric($maxAgeAllowed) && ($maxAgeAllowed >= 0)) {
-                if($minAgeAllowed > $maxAgeAllowed) {
-                    $tmp = $minAgeAllowed;
-                    $minAgeAllowed = $maxAgeAllowed;
-                    $maxAgeAllowed = $tmp;
-                }
-                
-                $yearsRange = range(date('Y')-$minAgeAllowed, date('Y')-$maxAgeAllowed);
+        $minAgeAllowed = min($options['minAgeAllowed'], $options['maxAgeAllowed']);
+        $maxAgeAllowed = max($options['minAgeAllowed'], $options['maxAgeAllowed']);
 
-                $this->formlyFieldConfiguration['templateOptions']['years'] = ($order == 'asc') ? array_reverse($yearsRange) : $yearsRange;
-            }
+        $yearsRange = range(date('Y')-$minAgeAllowed, date('Y')-$maxAgeAllowed);
 
-            unset($this->formlyFieldConfiguration['templateOptions']['minAgeAllowed']);
-            unset($this->formlyFieldConfiguration['templateOptions']['maxAgeAllowed']);
-        }
+        $this->formlyFieldConfiguration['templateOptions']['years'] = ($order == self::ORDER_ASC)
+            ? array_reverse($yearsRange)
+            : $yearsRange;
+
+        $this->cleanAgeAllowedInTemplateOptionsArray();
+    }
+
+    private function cleanAgeAllowedInTemplateOptionsArray(): void
+    {
+        unset($this->formlyFieldConfiguration['templateOptions']['minAgeAllowed']);
+        unset($this->formlyFieldConfiguration['templateOptions']['maxAgeAllowed']);
     }
 }
